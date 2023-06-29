38b43becb03b94782fd53a36881c5057ca57788f
kongheng <kongheng@glean.net>
2023-06-30 00:07:55 +0700

    add header and footer

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <h1>Header</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CustomHeader'
+    }
+</script>
+
+<style scoped>
+    .header {
+        background-color: blueviolet;
+        padding: 10px 0 10px 0;
+    }
+</style>
